eaf1716db421a3d059f7a51e539169342a6036cf
Dipak Niroula <dni@logpoint.com>
2024-01-16 14:14:03 +0545

    added integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use log::{info, error};
 use std::time::Instant;
-mod extractor;
+use rust_log_extractor::extractor::*;
 
 
 fn main() {
@@ -11,7 +11,7 @@ fn main() {
     // test sample 1
     let test_string_1 = "[INFO] User alice logged in from IP address 192.168.1.10.";
     let test_pattern_1 = "[<log_level:word>] User <username:word> logged in from IP address <ip_address:ip>.";
-    match extractor::extractor::parse_info(test_string_1, test_pattern_1) {
+    match extractor::parse_info(test_string_1, test_pattern_1) {
         Ok(matched) => {
             info!("Test String 1: {test_string_1}");
             info!("Test Pattern 1: {test_pattern_1}");
@@ -23,7 +23,7 @@ fn main() {
     // test sample 2
     let test_string_2 = "2024-01-14 12:34:56 [INFO] User alice logged in from IP address 192.168.1.10.";
     let test_pattern_2 = "<log_time:date_time> [<log_level:word>] User <username:word> logged in from IP address <ip_address:ip>.";
-    match extractor::extractor::parse_info(test_string_2, test_pattern_2) {
+    match extractor::parse_info(test_string_2, test_pattern_2) {
         Ok(matched) => {
             info!("Test String 2: {test_string_2}");
             info!("Test Pattern 2: {test_pattern_2}");
@@ -35,7 +35,7 @@ fn main() {
     // test sample 3
     let test_string_3 = "[INFO] User dipak logged in from IP address 136.24.10.44";
     let test_pattern_3 = "[<log_level:word>] User <username:word> logged in from IP address <ip_address:ip>.";
-    match extractor::extractor::parse_info(test_string_3, test_pattern_3) {
+    match extractor::parse_info(test_string_3, test_pattern_3) {
         Ok(matched) => {
             info!("Test String 3: {test_string_3}");
             info!("Test Pattern 3: {test_pattern_3}");
@@ -51,7 +51,7 @@ fn main() {
     for _ in 1..10001 {
         let test_string_3 = "[INFO] User dipak logged in from IP address 136.24.10.44";
         let test_pattern_3 = "[<log_level:word>] User <username:word> logged in from IP address <ip_address:ip>.";
-        match extractor::extractor::parse_info(test_string_3, test_pattern_3) {
+        match extractor::parse_info(test_string_3, test_pattern_3) {
             Ok(_) => success_result += 1,
             Err(_) => fail_result += 1
         }
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,11 +1,9 @@
-use rust_log_extractor::extractor::extractor::{parse_info, Matched};
-
-
+use rust_log_extractor::extractor::extractor::{Matched, parse_info};
 
 #[test]
 fn test_parse_info() {
     let test_string_1 = "[INFO] User alice logged in from IP address 192.168.1.10.";
     let test_pattern_1 = "[<log_level:word>] User <username:word> logged in from IP address <ip_address:ip>.";
-    let parsed_info: Vec<Matched> = parse_info(test_string_1, test_pattern_1);
+    let parsed_info = parse_info(test_string_1, test_pattern_1).unwrap();
     assert_eq!(parsed_info.len(), 3);
 }
